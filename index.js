4450285b3f1a5f274dca5911acf451a4ab7e8a91
Andrew Masih <armasih@outlook.com>
2021-06-03 23:20:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/* the || operator only requires 1 side to be true, in other words, the output will only be false if both sides are false */
+
+console.log(true || false); //true
+console.log(true || true); //true
+console.log(false || false); //false
+console.log(false || true); //true
+console.log((10<5) || (1>0)); // true 
+
+
